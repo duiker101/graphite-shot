11a611719c548f50245ae305591e9d691c831274
Ahmad Awais ⚡️ <me@AhmadAwais.com>
2020-04-17 19:36:20 +0500

    🐛 FIX: Extraneous useState
    Removed the extraneous `useState`.

--- src/components/toolbar/Toolbar.tsx
+++ src/components/toolbar/Toolbar.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from "react";
+import React, {useMemo} from "react";
 import styled from "styled-components";
 import ColorPicker from "../inputs/ColorPicker";
 import downloadImg from "../../icons/download.svg";
