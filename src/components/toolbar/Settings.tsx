3357ab3cd5070864a42c1130d9a8f28a275b317f
Simone Masiero <simone@duiker101.net>
2021-10-04 18:55:24 -0400

    cleanup and update libs

---
+++ src/components/toolbar/Settings.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+import {useDispatch} from "react-redux"
+
+const Wrapper = styled.div`
+`
+
+export default () => {
+	const dispatch = useDispatch();
+	return (
+		<Wrapper>
+		</Wrapper>
+	);
+}
--- src/components/toolbar/Toolbar.tsx
+++ src/components/toolbar/Toolbar.tsx
@@ -4,14 +4,18 @@ import ColorPicker from "../inputs/ColorPicker";
 import downloadImg from "../../icons/download.svg";
 import addImg from "../../icons/add.svg";
 import directionImg from "../../icons/direction.svg";
-import htmlToImage from "html-to-image";
+import {ReactComponent as CogImg} from "../../icons/cog.svg";
+import {toPng} from "html-to-image";
 import {saveAs} from "file-saver";
 import {useDispatch} from "react-redux";
 import {addWindow, useWindows} from "../../store/windows";
 import {AppDispatch} from "../../store";
 import SelectionToolbar from "./SelectionToolbar";
+import Settings from "./Settings";
 
-const Wrapper = styled.div`
+const Wrapper = styled.div``;
+
+const TopBar = styled.div`
 	display: flex;
 	justify-content: space-between;
 `;
@@ -74,49 +78,54 @@ export default ({
 
 	const save = () => {
 		if (!content) return;
-		htmlToImage
-			.toPng(content)
-			.then(dataUrl => {
+		toPng(content)
+			.then((dataUrl) => {
 				saveAs(dataUrl, "graphite.png");
 			})
-			.catch(error => {
+			.catch((error) => {
 				console.error("oops, something went wrong!", error);
 			});
 	};
 
 	return (
 		<Wrapper>
-			<Pickers>
-				<ColorPicker
-					color={bgColor}
-					onChange={onBgColor}
-					palette={[
-						"#3D7BC7",
-						"#17826D",
-						"#F7EBD1",
-						"#DFAC5D",
-						"#44B87E",
-					]}
-				/>
+			<TopBar>
+				<Pickers>
+					<Button>
+						<CogImg />
+					</Button>
+					<ColorPicker
+						color={bgColor}
+						onChange={onBgColor}
+						palette={[
+							"#3D7BC7",
+							"#17826D",
+							"#F7EBD1",
+							"#DFAC5D",
+							"#44B87E",
+						]}
+					/>
 
-				{Object.entries(windows).length > 1 && (
-					<DirectionButton
-						horizontal={horizontal}
-						onClick={() => onDirection(!horizontal)}>
-						<img alt={""} src={directionImg} />
-					</DirectionButton>
-				)}
-				<Button onClick={() => dispatch(addWindow())}>
-					<img alt={""} src={addImg} />
-				</Button>
-				<Divider />
-				<SelectionToolbar />
-			</Pickers>
-			<div>
-				<Button onClick={save}>
-					<img alt={""} src={downloadImg} />
-				</Button>
-			</div>
+					{Object.entries(windows).length > 1 && (
+						<DirectionButton
+							horizontal={horizontal}
+							onClick={() => onDirection(!horizontal)}>
+							<img alt={""} src={directionImg} />
+						</DirectionButton>
+					)}
+					<Button onClick={() => dispatch(addWindow())}>
+						<img alt={""} src={addImg} />
+					</Button>
+					<Divider />
+					<SelectionToolbar />
+				</Pickers>
+				<div>
+					<Button onClick={save}>
+						<img alt={""} src={downloadImg} />
+					</Button>
+				</div>
+			</TopBar>
+			<Settings />
 		</Wrapper>
 	);
 };
